7504012f16afc498253909234b7f86bd087ae55e
taro0727 <july.20200620@gmail.com>
2020-08-04 05:40:00 +0000

    validate&add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -49,8 +49,14 @@ class TasksController extends Controller
     //新規登録処理
     public function store(Request $request)
     {
+        //バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            ]);
+            
         //タスク作成
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -101,9 +107,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        //バリデーション
+        $request->validate([
+            'status' => 'required|max:10',
+            ]);
+            
         //idの値でタスクを検索、取得
         $task = Task::findOrFail($id);
         //タスク更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
