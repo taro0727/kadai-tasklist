2b6588762cd326daa276377f8f8433e7bf0bd84a
taro0727 <july.20200620@gmail.com>
2020-08-05 04:39:28 +0000

    revision index

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,11 +16,6 @@
             <tbody>
                 @foreach($tasks as $task)
                 <tr>
-                    <td>{{ $task->id }}</td>
-                    <td>{{ $task->status }}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                <tr>
                     {{-- タスク詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show',$task->id,['task' => $task->id]) !!}</td>
                     <td>{{ $task->status }}</td>
